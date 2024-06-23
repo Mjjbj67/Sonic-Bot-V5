a55aeb06ee61aa1f506d384ac6720a87d3d5c919
Mjjbj67 <165726694+Mjjbj67@users.noreply.github.com>
2024-06-23 11:37:26 +0100

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -1,4 +1,4 @@
-const fs = require("fs-extra");
+ const fs = require("fs-extra");
 const { utils } = global;
 
 module.exports = {
@@ -45,7 +45,7 @@ module.exports = {
                         confirmThisThread: "Please react to this message to confirm change prefix in your box chat",
                         successGlobal: "Changed prefix of system bot to: %1",
                         successThisThread: "Changed prefix in your box chat to: %1",
-                        myPrefix: "𝗛𝗘𝗟𝗟𝗢 𝗨𝗦𝗘𝗥 𝗜'𝗠 𝗔𝗖𝗧𝗜𝗩𝗘 𝗥𝗜𝗚𝗛𝗧 𝗡𝗢𝗪!! 𝗧𝗛𝗜𝗦 𝗜𝗦 𝗠𝗬 𝗣𝗥𝗘𝗙𝗜𝗫 ➪: /%1\n𝗖𝗛𝗔𝗧➪:  %2/𝗛𝗘𝗟𝗣 \n𝗧𝗢 𝗦𝗘𝗘 𝗔𝗟𝗟 𝗔𝗩𝗔𝗜𝗟 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦♡︎"
+                        myPrefix: "𝗛𝗘𝗟𝗟𝗢 𝗨𝗦𝗘𝗥 𝗜'𝗠 𝗔𝗖𝗧𝗜𝗩𝗘 𝗥𝗜𝗚𝗛𝗧 𝗡𝗢𝗪!! 𝗧𝗛𝗜𝗦 𝗜𝗦 𝗠𝗬 𝗣𝗥𝗘𝗙𝗜𝗫 ➪: %1\n𝗖𝗛𝗔𝗧➪:  %2𝗛𝗘𝗟𝗣 \n𝗧𝗢 𝗦𝗘𝗘 𝗔𝗟𝗟 𝗔𝗩𝗔𝗜𝗟 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦♡︎"
                 }
         },
 
