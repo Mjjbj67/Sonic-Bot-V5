1e9e4a606334590425dd9142a792575fb69d5797
Mjjbj67 <165726694+Mjjbj67@users.noreply.github.com>
2024-07-24 04:26:33 +0100

    Update callad.js

--- scripts/cmds/callad.js
+++ scripts/cmds/callad.js
@@ -56,7 +56,7 @@ module.exports = {
 		if (config.adminBot.length == 0)
 			return message.reply(getLang("noAdmin"));
 		const senderName = await usersData.getName(senderID);
-		const msg = "==📨️𝘾𝘼𝙇𝙇📬𝙎𝙊𝙉𝙄𝘾📨️=="
+		const msg = "==📨STANLEY•••BOT📨=="
 			+ `\n- User Name: 🍀${senderName}🍀`
 			+ `\n- User ID: 🎶${senderID}🎶`
 			+ (isGroup ? getLang("sendByGroup", (await threadsData.get(threadID)).threadName, threadID) : getLang("sendByUser"));
